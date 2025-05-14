ede2a840002156045fc5af6607873543d11f02e9
Kashish Pahuja <hello@digitalpaaji.com>
2025-05-14 17:02:58 +0530

    jn

--- frontend/src/app/components/story/Banner.js
+++ frontend/src/app/components/story/Banner.js
@@ -28,11 +28,11 @@ function Banner() {
 
   const words = ["Paaji’s ", "vision", "Your ", "growth"];
   return (
-    <div className="  w-full h-[800px] bg-[#FF850D] mx-4 md:mx-12 xl:mx-24 
+    <div className="  bg-[#FF850D] mx-4 md:mx-12 xl:mx-24 
     ">
-      <div className=" ">
-        <div className=" px-4">
-          <h1 className="laila-bold font-bold text-5xl md:text-7xl leading-tight tracking-tight">
+      <div className=" flex items-center justify-center">
+        <div className="text-center ">
+          <h1 className="laila-bold font-bold text-5xl md:text-6xl leading-tight tracking-tight">
         {words.map((word, index) => (
           <span
             key={index}
@@ -43,15 +43,15 @@ function Banner() {
           </span>
         ))}
       </h1>
-          <p className="text-xl mx-40 my-6">
+          <p className="text-xl poppins my-6 text-justify 2xl:mx-40">
           We were running a digital agency — building websites, running ads, managing brands. But one day, a college student asked us if he could learn from us. Then another. Then another.
 
           We realized — talent is everywhere, but guidance isn’t. That’s when Digital Paaji Academy was born.
 
     Paaji didn’t want to just build brands — he wanted to build people too.
           </p>
-          <p className="text-md sm:text-lg font-medium">
-            Paaji didn’t just build brands—he built people too.
+          <p className="poppins text-md sm:text-lg font-medium">
+            Paaji didn’t just build brands—he builds people too.
           </p>
         
         </div>
