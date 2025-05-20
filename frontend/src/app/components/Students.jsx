f4cbaf942e6741d0a76dc6c190fae899a74a1e80
Kashish Pahuja <hello@digitalpaaji.com>
2025-05-20 10:54:46 +0530

    b

--- frontend/src/app/components/Students.jsx
+++ frontend/src/app/components/Students.jsx
@@ -13,7 +13,7 @@ function Students() {
     '/videos/student2.mp4',
     '/videos/student3.mp4',
     '/videos/student4.mp4',
-    '/videos/student5.mp4',
+    // '/videos/student5.mp4',
     // Add more if needed
   ]
 
@@ -47,19 +47,19 @@ function Students() {
     <div className='mx-6 lg:mx-12 xl:mx-24 py-24'>
       {/* Text Row */}
       <div className='flex flex-col lg:flex-row justify-between items-start lg:items-center mb-16 gap-8'>
-        <h3 className="bungee-shade-regular text-4xl md:text-5xl xl:text-6xl font-bold leading-tight">
+        <h3 className="bungee-shade-regular text-4xl md:text-5xl xl:text-6xl font-bold leading-tight text-center lg:text-left ">
           Hear from Our Learners
         </h3>
       </div>
 
       {/* Video Grid */}
-      <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-5 gap-6'>
+      <div className='flex items-center justify-center flex-wrap lg:flex-nowrap gap-6'>
         {videos.map((video, index) => (
           <div
             key={index}
             // ref={el => (videosRef.current[index] = el)}
-            className={`overflow-hidden h-[250px] md:h-[380px] xl:h-[500px] rounded-2xl ${
-              index % 2 === 1 ? 'md:mt-12' : ''
+            className={`overflow-hidden w-[120px] sm:w-[250px] md:w-[200px] lg: xl:w-full h-[250px] md:h-[380px] xl:h-[500px]  ${
+              index % 2 === 1 ? 'lg:mt-12' : ''
             }`}
           >
             <video
@@ -68,7 +68,7 @@ function Students() {
               autoPlay
               muted
               loop
-              className='w-full h-full object-cover rounded-xl'
+              className='w-full h-full object-cover'
             />
           </div>
         ))}
