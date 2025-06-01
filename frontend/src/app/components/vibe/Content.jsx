bc344abb31d3522df0d50dc90858e7da465a1fa9
Kashish Pahuja <147261989+kashishpahuja@users.noreply.github.com>
2025-06-01 21:15:38 +0530

    x

--- frontend/src/app/components/vibe/Content.jsx
+++ frontend/src/app/components/vibe/Content.jsx
@@ -4,31 +4,32 @@ import Image from "next/image";
 
 function ReelCollage() {
   const mediaItems = [
-    { src: "/Images/about2.webp" },
-    { src: "/Images/about2.webp" },
-    { src: "/Images/about2.webp" },
-    { src: "/Images/about2.webp" },
-    { src: "/Images/about2.webp" },
-    { src: "/Images/about2.webp" },
-    { src: "/Images/about2.webp" },
-    { src: "/Images/about2.webp" },
+    { src: "/Images/vibe/1.mp4" },
+    { src: "/Images/vibe/2.mp4" },
+    { src: "/Images/vibe/3.mp4" },
+    { src: "/Images/vibe/4.mp4" },
+    { src: "/Images/vibe/5.mp4" },
+    { src: "/Images/vibe/6.mp4" },
+   
     // Add more reel-style images
   ];
 
   return (
-    <div className="w-full px-2 sm:px-6 md:px-12 py-12 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+    <div className="w-full px-2 sm:px-6 md:px-12 py-12 flex items-center justify-center flex-wrap gap-6">
       {mediaItems.map((item, index) => (
         <div
           key={index}
-          className="relative aspect-[9/16] overflow-hidden rounded-2xl shadow-lg"
+          className="relative overflow-hidden"
         >
-          <Image
-            src={item.src}
-            alt={`reel-image-${index}`}
-            fill
-            sizes="(max-width: 768px) 50vw, (max-width: 1024px) 33vw, 25vw"
-            className="object-cover transition-transform duration-300 hover:scale-105"
-          />
+           <video
+  src={item.src}
+  loading="lazy"
+  autoPlay
+  loop
+  muted
+  playsInline
+  className="w-full h-full object-cover"
+/>
         </div>
       ))}
     </div>
