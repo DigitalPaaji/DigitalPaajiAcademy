872d4b8b3a1da0ba30dbbb414d68e69b1bef4d9e
DigitalPaaji <197368936+DigitalPaaji@users.noreply.github.com>
2025-07-26 10:25:01 +0530

    july 26

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -5,17 +5,39 @@ import NavPage from "./components/NavPage";
 
 
 
-export const metadata = {
-  title: "Digital Paaji Academy",
-  description: "Gyaan - Kam, Skills Zyada ! Focused on practical learning over theory, we help you master real-world skills for success.",
-};
+// export const metadata = {
+//   title: "Digital Paaji Academy",
+//   description: "Gyaan - Kam, Skills Zyada ! Focused on practical learning over theory, we help you master real-world skills for success.",
+// };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-       <head>
+        <head>
         <link rel="icon" href="/favicon.webp" sizes="any" />
-      
+     <title>Digital Paaji Academy</title>
+
+        <meta
+          name="description"
+          content="Gyaan - Kam, Skills Zyada! Focused on practical learning over theory, we help you master real-world skills for success."
+        />
+     
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "WebSite",
+              "name": "Digital Paaji Academy",
+              "url": "https://digitalpaajiacademy.com/",
+              "potentialAction": {
+                "@type": "SearchAction",
+                "target": "https://digitalpaajiacademy.com/?s={search_term_string}",
+                "query-input": "required name=search_term_string"
+              }
+            }),
+          }}
+        />
       </head>
 
       <body 
