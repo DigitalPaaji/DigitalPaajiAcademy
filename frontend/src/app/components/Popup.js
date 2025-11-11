970a1367ddb5bd47cc3d42f2ded6274bd242c745
DigitalPaaji <147261989+kashishpahuja@users.noreply.github.com>
2025-11-11 14:03:35 +0530

    bv

--- frontend/src/app/components/Popup.js
+++ frontend/src/app/components/Popup.js
@@ -75,7 +75,7 @@ function Popup({ closeMenu }) {
         setIsSubmitting(true);
     
         try {
-          const response = await fetch('https://digitalpaajiacademy.onrender.com/send-mail', {
+          const response = await fetch('http://localhost:8000/send-mail', {
          
     
             method: 'POST',
